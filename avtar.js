d2e3745ea51d4f1d83e75efae20efa4572c2b28a
Mangaldeep Das <39870866+liku88@users.noreply.github.com>
2020-04-29 09:03:17 +0530

    Update avtar.js

--- avtar.js
+++ avtar.js
@@ -1,13 +1,4 @@
-//By jquery
 
-//$(document).ready(function(){
-//    $('.but').click(function(){
-//       const name = $('#name').val();
-//       $('#imgid').attr('src', `https://joeschmoe.io/api/v1/${name}`);
-//    });
-//});
-
-// By javascript
 
 function fun(){
     var name = document.getElementById('name').value;
